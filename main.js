095ede7d80111eed353ac60e22983fa1eec7b452
Shriram-Hangarge <shriramhangarge4@gmail.com>
2024-06-14 12:58:46 +0530

    index.html updates with chatbot

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log(`kello kunal`);
+
+
+// Adding products to cart
+document.getElementById("cart-btn").addEventListener("click",addpro)
+function addpro(){
+        alert("item added to cart");
+}
+
